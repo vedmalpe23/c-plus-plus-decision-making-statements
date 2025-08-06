ec5ba385b5a34846bdcceac02f9f4829a05f5a3b
ved malpe <vedmalpe21@gmail.com>
2025-08-07 00:23:47 +0530

    Create program2.cpp

---
+++ program2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    char a ;
+    cout<<"enter the letter";
+    cin>>a;
+    if(a=='a'||a =='e'||a=='i'||a =='o'||a =='u'){
+        cout<<("the letter is a vowel");
+    }
+    else{
+        cout<<"the letter is a constant";
+    }
+    return 0;
+}
+    
